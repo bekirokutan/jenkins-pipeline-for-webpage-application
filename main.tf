b1e8eef444f5c4b9c4851439b0e32c492e24155c
Blgsyrc0560 <113426804+Blgsyrc0560@users.noreply.github.com>
2023-05-20 23:26:12 +0300

    ansible playbook modifed

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "jenkins-project-backend-okutan"
+    bucket = "jenkins-project-backend-o"
     key = "backend/tf-backend-jenkins.tfstate"
     region = "us-east-1"
   }
@@ -14,37 +14,38 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  #  secret_key = ""
-  #  access_key = ""
 }
 
-resource "aws_instance" "nodes" {
-  ami = var.myami
-  instance_type = var.instancetype
-  count = var.num
-  key_name = var.mykey
+variable "tags" {
+  default = ["postgresql", "nodejs", "react"]
+}
+
+variable "user" {
+  default = "oliver"
+}
+
+resource "aws_instance" "managed_nodes" {
+  ami = "ami-016eb5d644c333ccb"
+  count = 3
+  instance_type = "t2.micro"
+  key_name = "project-208"  # change with your pem file
   vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
-  iam_instance_profile = "jenkins-project-profile-${var.user}"
+  iam_instance_profile = "jenkins-project-profile-${var.user}" # we created this with jenkins server
   tags = {
     Name = "ansible_${element(var.tags, count.index )}"
     stack = "ansible_project"
     environment = "development"
   }
-  user_data = <<-EOF
+    user_data = <<-EOF
               #! /bin/bash
               dnf update -y
               EOF
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 resource "aws_security_group" "tf-sec-gr" {
-  name = "${var.mysecgr}-${var.user}"
-  vpc_id = data.aws_vpc.default.id
+  name = "project208-sec-gr-${var.user}"
   tags = {
-    Name = var.mysecgr
+    Name = "project208-sec-gr"
   }
 
   ingress {
@@ -81,13 +82,13 @@ resource "aws_security_group" "tf-sec-gr" {
 }
 
 output "react_ip" {
-  value = "http://${aws_instance.nodes[2].public_ip}:3000"
+  value = "http://${aws_instance.managed_nodes[2].public_ip}:3000"
 }
 
 output "node_public_ip" {
-  value = aws_instance.nodes[1].public_ip
+  value = aws_instance.managed_nodes[1].public_ip
 }
 
 output "postgre_private_ip" {
-  value = aws_instance.nodes[0].private_ip
+  value = aws_instance.managed_nodes[0].private_ip
 }
